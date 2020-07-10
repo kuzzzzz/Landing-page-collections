2ca3cbb568cc609975057406a6d7ef6dd273aa9d
Atumah Daniel <atumahdaniel@gmail.com>
2020-07-10 01:40:23 +0100

    sign up form

---
+++ util/sign-up-form/index.js
@@ -0,0 +1,5 @@
+const form = document.getElementById('form');
+firstname = document.getElementById('firstname')
+lastname = document.getElementById('lastname')
+email = document.getElementById('email')
+password = document.getElementById('password')
