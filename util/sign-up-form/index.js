f8f6ecfbc1534da01e43b78a9460ebbfe5aa10dd
Atumah Daniel <atumahdaniel@gmail.com>
2020-07-11 01:32:47 +0100

    form validation utility component

--- util/sign-up-form/index.js
+++ util/sign-up-form/index.js
@@ -1,5 +1,53 @@
-const form = document.getElementById('form');
-firstname = document.getElementById('firstname')
-lastname = document.getElementById('lastname')
-email = document.getElementById('email')
-password = document.getElementById('password')
+const form = document.getElementById("form");
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+
+  const firstName = form["firstname"].value;
+  const lastName = form["lastname"].value;
+  const email = form["email"].value;
+  const password = form["password"].value;
+
+  if (firstName === "") {
+    addErrorClass("firstname", "First Name is required");
+  } else {
+    removeErrorClass("firstname");
+  }
+  if (lastName === "") {
+    addErrorClass("lastname", "Last Name is required");
+  } else {
+    removeErrorClass("lastname");
+  }
+  if (!validateEmail(email)) {
+    addErrorClass("email", "Email is not Valid");
+  } else {
+    removeErrorClass("email");
+  }
+  if (password === "") {
+    addErrorClass("password", "Password is required");
+  } else {
+    removeErrorClass("password");
+  }
+});
+
+function addErrorClass(field, message) {
+  const formControl = form[field].parentNode;
+  formControl.classList.add("error");
+  const small = formControl.querySelector("small");
+  if (field === "email") {
+    document.getElementById("email").placeholder = "email@example.com";
+  }
+  small.innerText = message;
+  small.style.opacity = "1";
+}
+
+function removeErrorClass(field) {
+  const formControl = form[field].parentNode;
+  formControl.classList.remove("error");
+  const small = formControl.querySelector("small");
+  small.style.opacity = "0";
+}
+function validateEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
