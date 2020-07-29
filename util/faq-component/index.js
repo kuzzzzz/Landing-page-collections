0c8ca1fdad03e0946879714a2e010d6e6c279e5e
Atumah Daniel <atumahdaniel@gmail.com>
2020-07-29 21:22:14 +0100

    accordion using scss and vanilla js

--- util/faq-component/index.js
+++ util/faq-component/index.js
@@ -1,15 +1,30 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
+// var acc = document.getElementsByClassName("accordion");
+// var i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
+// for (i = 0; i < acc.length; i++) {
+//   acc[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var panel = this.nextElementSibling;
+//     if (panel.style.display === "block") {
+//       panel.style.display = "none";
+//     } else {
+//       panel.style.display = "block";
+//     }
+//   });
+// }
+
+const accordionItem = document.querySelectorAll(".accordion-item");
+
+accordionItem.forEach((el) =>
+  el.addEventListener("click", () => {
+    if (el.classList.contains("active")) {
+      el.classList.remove("active");
     } else {
-      panel.style.display = "block";
+      accordionItem.forEach((el2) => el2.classList.remove("active"));
+      el.classList.add("active");
     }
-  });
-}
+  })
+);
+
+
 
