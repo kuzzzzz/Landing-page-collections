8a9ebb412b736da7aaaac2672942943ac62d3e31
Atumah Daniel <atumahdaniel@gmail.com>
2020-08-02 01:58:38 +0100

    product slider

--- landing-pages/bootstrap-store/index.js
+++ landing-pages/bootstrap-store/index.js
@@ -8,3 +8,13 @@ $(".slider-one")
     prevArrow:".site-slider .slider-btn .prev",
     nextArrow:".site-slider .slider-btn .next",
   });
+$(".slider-two")
+  .not(".slick-initialized")
+  .slick({
+ 
+    prevArrow:".site-slider-two .prev",
+    nextArrow:".site-slider-two .next",
+    slidesToShow:4,
+    slidesToScroll:1,
+    autoplay:2000
+  });
