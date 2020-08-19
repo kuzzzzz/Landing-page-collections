0e3b5f5806de051aaf59d526c56b02e7a6f07173
Atumah Daniel <atumahdaniel@gmail.com>
2020-08-19 01:23:38 +0100

    top navigation

--- landing-pages/the-rosa/main.js
+++ landing-pages/the-rosa/main.js
@@ -1,1 +1,12 @@
 //Select element function
+
+const selectElment = function(elm) {
+    return document.querySelector(elm)
+}
+
+let menuToggler = selectElment('.menu-toggle')
+let body = selectElment('body')
+
+menuToggler.addEventListener('click', function () {
+  body.classList.toggle('open');  
+})
