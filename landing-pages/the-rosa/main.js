6154867946c7561a9d9d675b17b233a5fedbe5d0
Atumah Daniel <atumahdaniel@gmail.com>
2020-08-23 00:01:31 +0100

    added animate on scroll animations with javascript

--- landing-pages/the-rosa/main.js
+++ landing-pages/the-rosa/main.js
@@ -1,12 +1,40 @@
 //Select element function
 
 const selectElment = function(elm) {
-    return document.querySelector(elm)
-}
+  return document.querySelector(elm);
+};
 
-let menuToggler = selectElment('.menu-toggle')
-let body = selectElment('body')
+let menuToggler = selectElment(".menu-toggle");
+let body = selectElment("body");
 
-menuToggler.addEventListener('click', function () {
-  body.classList.toggle('open');  
-})
+menuToggler.addEventListener("click", function() {
+  body.classList.toggle("open");
+});
+
+// scroll reveal
+window.sr = ScrollReveal();
+
+sr.reveal(".animate-left", {
+  origin: "left",
+  duration: 1000,
+  distance: "25rem",
+  delay: 300
+});
+sr.reveal(".animate-right", {
+  origin: "right",
+  duration: 1000,
+  distance: "25rem",
+  delay: 600
+});
+sr.reveal(".animate-top", {
+  origin: "top",
+  duration: 1000,
+  distance: "25rem",
+  delay: 600
+});
+sr.reveal(".animate-bottom", {
+  origin: "bottom",
+  duration: 1000,
+  distance: "25rem",
+  delay: 600
+});
