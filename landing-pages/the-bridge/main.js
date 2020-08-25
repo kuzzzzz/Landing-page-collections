8306c10a3c269d72e52ecf6071182c049cd8301e
Atumah Daniel <atumahdaniel@gmail.com>
2020-08-25 03:52:21 +0100

    completed lp

--- landing-pages/the-bridge/main.js
+++ landing-pages/the-bridge/main.js
@@ -1,9 +1,25 @@
-const mainNav =  document.querySelector('.main-nav');
-const navList =  document.querySelector('.nav-list');
-const hamburgerMenu =  document.querySelector('.hamburger-menu');
+const mainNav = document.querySelector(".main-nav");
+const navList = document.querySelector(".nav-list");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
 
-hamburgerMenu.addEventListener('click', function () {
-    mainNav.classList.toggle('open')
-    console.log("lolr");
-})
+hamburgerMenu.addEventListener("click", function() {
+  mainNav.classList.toggle("open");
+  console.log("lolr");
+});
 
+gsap.fromTo(".hero-clipped", { scaleX: 0 }, { duration: 1, scaleX: 1 });
+gsap.fromTo(
+  ".logo",
+  { x: -200, opacity: 0 },
+  { duration: 1, delay: 0.5, x: 0, opacity: 1 }
+);
+gsap.fromTo(
+  ".hamburger-menu",
+  { x: 200, opacity: 0 },
+  { duration: 1, delay: 0.8, x: 0, opacity: 1 }
+);
+gsap.fromTo(
+  ".hero-textbox",
+  { yPercent: 40, opacity: 0 },
+  { duration: 1, delay: 1.3, yPercent:-50, opacity: 1 }
+);
