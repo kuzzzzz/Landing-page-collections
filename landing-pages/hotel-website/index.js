2c248558dfef8abcb2e66f00a075223e73724bc9
Atumah Daniel <atumahdaniel@gmail.com>
2020-09-29 12:24:31 +0100

    Footer styles and media queries

--- landing-pages/hotel-website/index.js
+++ landing-pages/hotel-website/index.js
@@ -1,6 +1,10 @@
-let header = document.querySelector('.header');
-let hamburgerMenu = document.querySelector('.hamburger-menu')
+let header = document.querySelector(".header");
+let hamburgerMenu = document.querySelector(".hamburger-menu");
 
-hamburgerMenu.addEventListener('click',function () {
-    header.classList.toggle('menu-open')
-})
+window.addEventListener("scroll", function () {
+  let windowPosition = window.scrollY > 0;
+  header.classList.toggle("active", windowPosition);
+});
+hamburgerMenu.addEventListener("click", function () {
+  header.classList.toggle("menu-open");
+});
